ccda3a5bb58e528f6a28c4e123a2f63a2d0a9300
codinronan <patrick@rolamix.com>
2018-06-06 04:22:02 -0500

    Use express compression

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const stencil = require('@stencil/core/server');
+const compression = require('compression');
 
 // create the express app
 const app = express();
+
+app.use(compression());
 
 // set which port express it will be using
 const port = process.env.PORT || 3030;
 
 // load the stencil config & init server-side rendering html pages
-const config = stencil.initApp({ // stencil.loadConfig(__dirname);
-  app: app,
-  configPath: __dirname
+const config = stencil.initApp({
+  app,
+  configPath: __dirname,
 });
 
 // serve all static files from www directory
