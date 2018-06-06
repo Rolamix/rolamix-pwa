9c319cd86279095cd8c71e717af2f0e441f65090
codinronan <patrick@rolamix.com>
2018-06-06 04:21:53 -0500

    Use postcss for css variables with scss

---
+++ src/global/index.ts
@@ -0,0 +1,13 @@
+import { initServiceWorker } from '../utils/sw-init';
+// import Services from '../services';
+
+// This is temporarily commented out as the `setupConfig` method has been temporarily removed
+// setupConfig({
+  // uncomment the following line to force mode to be Material Design
+  // mode: 'md'
+// });
+
+initServiceWorker();
+
+// declare var Context: any;
+// Context.Services = Services;
--- stencil.config.js
+++ stencil.config.js
@@ -1,4 +1,6 @@
 const sass = require('@stencil/sass');
+const postcss = require('@stencil/postcss');
+const cssvariables = require('postcss-css-variables');
 
 exports.config = {
   namespace: 'app',
@@ -12,9 +14,17 @@ exports.config = {
   enableCache: false,
   srcDir: 'src',
   // globalStyle: 'src/global/app.css', // don't use this if you can keep from it.
-  globalScript: 'src/global/app.ts',
+  globalScript: 'src/global/index.ts',
   plugins: [
-    sass()
+    postcss({
+      // Transform css-variable usages to use fallbacks for older browsers
+      plugins: [cssvariables({
+        preserve: true,
+      })]
+    }),
+    sass({
+      injectGlobalPaths: [ ] // SASS variables, mixins & functions only.
+    }),
   ],
   // hashFileNames: true,
   // hashedFileNameLength: 8,
@@ -22,10 +32,11 @@ exports.config = {
     {
       type: 'www',
       console: console,
+      logLevel: 'debug',
       serviceWorker: {
         swSrc: 'src/sw.js',
         globPatterns: [
-          '**/*.{js,css,json,html,ico,png,svg,jpg}'
+          '**/*.{js,css,json,html,ico,png,svg,jpg,jpeg}'
         ],
       }
     }
