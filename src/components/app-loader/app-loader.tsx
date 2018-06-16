ff122cf63b425ab394a8b9551f569653d427db64
codinronan <patrick@rolamix.com>
2018-06-16 14:40:40 -0500

    refactor(app-loader): remove unneeded code, shorten functions

--- src/components/app-loader/app-loader.tsx
+++ src/components/app-loader/app-loader.tsx
@@ -1,6 +1,6 @@
+import 'ionicons'; // tslint:disable-line no-import-side-effect
 import '@ionic/core'; // tslint:disable-line no-import-side-effect
 import { Component, Listen, Prop, State } from '@stencil/core';
-// import { State } from '@stencil/core';
 
 @Component({
   tag: 'app-loader',
@@ -8,19 +8,33 @@ import { Component, Listen, Prop, State } from '@stencil/core';
 })
 export class AppLoader {
 
-  @Prop({ connect: 'ion-toast-controller' }) toastCtrl: HTMLIonToastControllerElement;
-  @Prop({ context: 'document' }) private doc: Document;
-  @State() isMenuOpen: boolean = false;
   // @Prop({ context: 'window' }) private window: Window;
-  // @State() private isLoading = true;
+  @Prop({ context: 'document' }) private doc: Document;
+  @Prop({ connect: 'ion-toast-controller' }) toastCtrl: HTMLIonToastControllerElement;
+  @State() private isMenuOpen: boolean = false;
+  // @State() private isLoading = true; // only set to true if on client.
 
-  // public componentWillLoad(): void {
-  //   if ('serviceWorker' in this.window.navigator) {
-  //     this.window.navigator.serviceWorker.ready
-  //       .then(() => (this.isLoading = false))
-  //       .catch(() => (this.isLoading = false));
-  //   }
-  // }
+  /**
+   * Handle service worker updates correctly.
+   * This code will show a toast letting the user of the PWA know that there is a
+   * new version available. When they click the reload button it then reloads the
+   * page so that the new service worker can take over and serve the fresh content
+   * Alternatively you can watch this.window.navigator.serviceWorker.ready
+   */
+  @Listen('window:swUpdate')
+  async onSWUpdate(event: CustomEvent) {
+    // this.isLoading = false;
+    if (event.detail.type === 'refresh') { // also 'cached'
+      const toast = await this.toastCtrl.create({
+        message: 'New version available',
+        showCloseButton: true,
+        closeButtonText: 'Update Now!'
+      });
+      await toast.present();
+      await toast.onWillDismiss();
+      window.location.reload();
+    }
+  }
 
   @Listen('menuToggle')
   handleMenuToggle(event: CustomEvent) {
@@ -37,28 +51,6 @@ export class AppLoader {
       this.doc.body.classList.add('with--sidebar');
     } else {
       this.doc.body.classList.remove('with--sidebar');
-    }
-  }
-
-  /**
-   * Handle service worker updates correctly.
-   * This code will show a toast letting the user of the PWA know that there is a
-   * new version available. When they click the reload button it then reloads the
-   * page so that the new service worker can take over and serve the fresh content
-   */
-  @Listen('window:swUpdate')
-  async onSWUpdate(event: CustomEvent) {
-    console.log('swUpdate: ', event.detail.type);
-    // this.isLoading = false;
-    if (event.detail.type === 'refresh') { // also 'cached'
-      const toast = await this.toastCtrl.create({
-        message: 'New version available',
-        showCloseButton: true,
-        closeButtonText: 'Update Now!'
-      });
-      await toast.present();
-      await toast.onWillDismiss();
-      window.location.reload();
     }
   }
 
@@ -91,21 +83,14 @@ export class AppLoader {
 
     return (
       <stencil-lift>
-
         <site-header isOpen={this.isMenuOpen} />
 
         <div class="site-container">
-          {/* <div class="site-pusher"> */}
+          <main class="site-content no-push">
+            {this.renderThRouter()}
+          </main>
 
-            <main class="site-content no-push">
-              {/* <div class="container"> */}
-                {this.renderThRouter()}
-              {/* </div> */}
-            </main>
-
-            <div class="backdrop" onClick={this.handleBackdropClick} />
-
-          {/* </div> */}
+          <div class="backdrop" onClick={this.handleBackdropClick} />
         </div>
       </stencil-lift>
     );
